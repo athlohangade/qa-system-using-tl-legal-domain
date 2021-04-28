d89a8701fabec1fe34cd174738938adf49931a4c
athlohangade <athlohangade@gmail.com>
2021-04-28 23:51:32 +0530

    Middleware functions updated

--- Main.py
+++ Main.py
@@ -30,5 +30,20 @@ class Main :
 
         return answers
 
+    @classmethod
+    def get_contexts_given_the_doc(cls, doc_name) :
+
+        contexts = TextProcessingAndContextCreation.get_context_chunks(doc_name)
+        return contexts
 
 
+    @classmethod
+    def get_questions_given_the_contexts(cls, context_list) :
+
+        generated_questions_and_contexts = []
+        for context in context_list :
+            questions = cls.qg(context)
+            for j in questions :
+                generated_questions_and_contexts.append([j['question'], context])
+
+        return generated_questions_and_contexts
