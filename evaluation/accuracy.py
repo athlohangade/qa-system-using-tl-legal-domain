babae59689fcad1bc88d0542edf04508c43feb93
atharavmundada <atharavmundada@gmail.com>
2021-05-09 16:30:07 +0530

    Fine-tuning code

--- evaluation/accuracy.py
+++ evaluation/accuracy.py
@@ -1,41 +1,72 @@
+#Evaluation Metric Function
 import re
 from collections import Counter
 
-def calculate_accuracy(true, predictions) :
-
+#Function to compute the Accuracy and the F1 score
+#Predictions -> Predictions by the model
+#References -> Reference items, to compare with the predictions
+def compute(predictions, references):
     predictions_score = 0
-    true_score = 0
+    common_cnt = 0
+    predicted_cnt = 0
+    reference_cnt = 0
+    sample_pred = list(predictions)
+    sample_ref = list(references)
+    class_cnt = [0, 0, 0, 0, 0]
 
-    for i in range(len(true)) :
-        true[i] = true[i].lower()
-        predictions[i] = predictions[i].lower()
+    for i in range(len(references)) :
+        #Converting all the lower case
+        sample_ref[i] = sample_ref[i].lower()
+        sample_pred[i] = sample_pred[i].lower()
 
-        true[i] = re.sub(r'[^\w\s]', '', true[i])
-        predictions[i] = re.sub(r'[^\w\s]', '', predictions[i])
+        #Removing the punctuations
+        sample_ref[i] = re.sub(r'[^\w\s]', ' ', sample_ref[i])
+        sample_pred[i] = re.sub(r'[^\w\s]', ' ', sample_pred[i])
 
-        true[i] = true[i].split()
-        predictions[i] = predictions[i].split()
+        #Fetching the individual tokens
+        sample_ref[i] = sample_ref[i].split()
+        sample_pred[i] = sample_pred[i].split()
 
-        match_len = len(list((Counter(true[i]) & Counter(predictions[i])).elements())) 
-        match_ratio = match_len / len(true[i])
+        #Finding the number of common words between the predicted item and the reference item
+        cnt_common = sum(( Counter(sample_ref[i]) & Counter(sample_pred[i]) ).values())
 
+        #Evaluating the difference counts for f1 measurement
+        common_cnt += cnt_common
+        predicted_cnt += len(sample_pred[i])
+        reference_cnt += len(sample_ref[i])
+        #Evaluating the match fraction of the prediction with the reference
+        match_ratio = cnt_common / len(sample_ref[i])
+
+        #Evaluating the scores based on the amount of match
         if match_ratio >= 0.25 and match_ratio < 0.5 :
             predictions_score += 1
+            class_cnt[1] += 1
         elif match_ratio >= 0.5 and match_ratio < 0.75 :
             predictions_score += 2
+            class_cnt[2] += 1
         elif match_ratio >= 0.75 and match_ratio < 1 :
             predictions_score += 3
+            class_cnt[3] += 1
         elif match_ratio == 1 :
             predictions_score += 4
+            class_cnt[4] += 1
+        else :
+            class_cnt[0] += 1
 
-    true_score = 4 * len(true)
-    accuracy = predictions_score / true_score
-    return accuracy
+    #Total score for references
+    reference_score = 4 * len(sample_ref)
 
+    #Various Evaluation Metrics
+    accuracy = predictions_score / reference_score * 100
+    precision = common_cnt / predicted_cnt
+    recall = common_cnt / reference_cnt
+    F1 = ((2 * precision * recall) / (precision + recall)) * 100
 
-if __name__ == '__main__' :
-
-    true = ['My , name is Atharva', 'I like to play football.']
-    predictions = ['My ; name is Vishal', 'I like tennis;']
-    acc = calculate_accuracy(true, predictions)
-    print(acc)
+    #Sending the result
+    metric = dict()
+    metric['accuracy'] = accuracy
+    metric['precision'] = precision
+    metric['recall'] = recall
+    metric['f1'] = F1
+    metric['class_cnt'] = class_cnt
+    return metric
