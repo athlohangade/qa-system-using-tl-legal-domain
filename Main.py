10b9f75020f9eb9d8bf0185d53e3facd1c0c7d27
athlohangade <athlohangade@gmail.com>
2021-05-11 02:13:11 +0530

    Added function for incorporate mode 2 operation

--- Main.py
+++ Main.py
@@ -10,27 +10,6 @@ class Main :
     qa = QA()
 
     @classmethod
-    def get_questions_and_contexts_given_the_doc(cls, doc_name) :
-
-        contexts = TextProcessingAndContextCreation.get_context_chunks(doc_name)
-        generated_questions_and_contexts = []
-        for i in contexts :
-            questions = cls.qg(i)
-            for j in questions :
-                generated_questions_and_contexts.append([j['question'], i])
-
-        return generated_questions_and_contexts
-
-    @classmethod
-    def get_answer_given_the_question_and_context_list(cls, question_context_list) :
-
-        answers = []
-        for i in question_context_list :
-            answers.append(cls.qa.answer([i[0]], [i[1]]))
-
-        return answers
-
-    @classmethod
     def get_contexts_given_the_doc(cls, doc_name) :
 
         contexts = TextProcessingAndContextCreation.get_context_chunks(doc_name)
@@ -47,3 +26,21 @@ class Main :
                 generated_questions_and_contexts.append([j['question'], context])
 
         return generated_questions_and_contexts
+
+
+    @classmethod
+    def get_answer_given_the_question_and_context_list(cls, question_context_list) :
+
+        answers = []
+        for i in question_context_list :
+            answers.append(cls.qa.answer([i[0]], [i[1]]))
+
+        return answers
+
+
+    @classmethod
+    def get_answer_for_single_question_given_context_list(cls, question, context_list) :
+
+        repeated_question_list = [question] * len(context_list)
+        answer = cls.qa.answer(repeated_question_list, context_list)
+        return answer
