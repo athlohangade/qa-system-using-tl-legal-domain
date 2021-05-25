65a08da9b785c7e41b4c4c4b0eb2e734df1fbb32
athlohangade <athlohangade@gmail.com>
2021-05-25 16:41:24 +0530

    Commented the UI code and added the deleted uploads folder

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+# Import required libraries
 import os 
 import sys
 sys.path += ['./']
@@ -5,34 +6,37 @@ import random
 import streamlit as st
 from Main import Main
 
-
+# Function to get the contexts given the doc name. Returns the cached entry if available,
+# else make the required function call
 @st.cache(suppress_st_warning=True, allow_output_mutation=True, show_spinner=False)
 def get_contexts(doc_name) :
 
     contexts = Main.get_contexts_given_the_doc(doc_name)
     return contexts
 
-
+# Function to generate the questions given the contexts. Returns the cached entry if available,
+# else make the required function call
 @st.cache(suppress_st_warning=True, allow_output_mutation=True, show_spinner=False)
 def generate_questions(contexts) :
 
     generated_questions_and_contexts = Main.get_questions_given_the_contexts(contexts)
     return generated_questions_and_contexts
 
-
+# Function to find the answers given the contexts and questions. Returns the cached entry if available,
+# else make the required function call. Machine generated questions case.
 @st.cache(suppress_st_warning=True, allow_output_mutation=True, show_spinner=False)
 def get_answers_for_questions(generated_questions_and_contexts) :
 
     answers = Main.get_answer_given_the_question_and_context_list(generated_questions_and_contexts)
     return answers
 
-
+# Function to find the answer given a question and list of contexts. Returns the cached entry if available,
+# else make the required function call. User inputted questions case.
 @st.cache(suppress_st_warning=True, allow_output_mutation=True, show_spinner=False)
 def generate_answer_for_the_question(question, contexts) :
 
     answer = Main.get_answer_for_single_question_given_context_list(question, contexts)
     return answer
-
 
 
 # Print the title
@@ -44,31 +48,39 @@ st.header("Select the mode of operation :")
 modes = ['Generate and Answer Question on Document', 'Answer Question on Document']
 mode_of_operation = st.radio("", range(len(modes)), format_func = lambda x: modes[x])
 
+# If first mode is selected
 if mode_of_operation == 0 :
 
-    # Print the dropdown list for selecting the doc
     st.header("Select a Legal Document :")
     st.write(" ")
+    # Get the legal acts titles
     legal_docs_names = os.listdir("./legal_docs/")
     legal_docs_names = [i for i in legal_docs_names if i[-4:] == ".pdf"]
     legal_docs_names = ["Select Act"] + legal_docs_names
+    # Dropdown list widget for selecting the document
     doc_name = st.selectbox("", [legal_doc.replace(".pdf", "") for legal_doc in legal_docs_names])
 
+    # Upload widget for letting the user upload the document
     uploaded_file = st.file_uploader("Choose a file", type = 'pdf')
     if uploaded_file is not None :
+        # Save the file on our server
         with open(f"./legal_docs/uploads/{uploaded_file.name}", "wb") as f:
             f.write(uploaded_file.getbuffer())
         doc_name = "uploads/" + uploaded_file.name[:-4]
 
+    # If user selects any document
     if doc_name != "Select Act" :
 
+        # Get the contexts from the selected document
         doc_name = "./legal_docs/" + doc_name + ".pdf"
         contexts = get_contexts(doc_name)
 
+        # If user wants more samples, shuffle the contexts to skip the cached entries
         st.write(" ")
         if st.button("Generate more samples") :
             random.shuffle(contexts)
 
+        # Use the first 3 contexts of the list for generating questions and answers
         contexts = contexts[:3]
         with st.spinner("Generating Questions...") :
             generated_questions_and_contexts = generate_questions(contexts)
@@ -94,34 +106,42 @@ if mode_of_operation == 0 :
         st.header("Context :")
         st.markdown("---\n" f"{generated_questions_and_contexts[selected_question][1]}\n\n" "---")
 
-
+# If second mode is selected
 elif mode_of_operation == 1 :
 
-     # Print the dropdown list for selecting the doc
     st.header("Select a Legal Document :")
     st.write(" ")
+    # Get the legal acts titles
     legal_docs_names = os.listdir("./legal_docs/")
     legal_docs_names = [i for i in legal_docs_names if i[-4:] == ".pdf"]
     legal_docs_names = ["Select Act"] + legal_docs_names
+    # Dropdown list widget for selecting the document
     doc_name = st.selectbox("", [legal_doc.replace(".pdf", "") for legal_doc in legal_docs_names])
 
+    # Upload widget for letting the user upload the document
     uploaded_file = st.file_uploader("Choose a file", type = 'pdf')
     if uploaded_file is not None :
+        # Save the file on our server
         with open(f"./legal_docs/uploads/{uploaded_file.name}", "wb") as f:
             f.write(uploaded_file.getbuffer())
         doc_name = "uploads/" + uploaded_file.name[:-4]
 
+    # If user selects any document
     if doc_name != "Select Act" :
 
+        # UI widget to input the question from the user
         st.header("Enter the Question :")
         question = st.text_area("")
 
+        # Get the contexts from the selected document
         doc_name = "./legal_docs/" + doc_name + ".pdf"
         contexts = get_contexts(doc_name)
 
+        # Find the answer to the user's question
         st.write(" ")
         if st.button("Get the Answer") :
 
+            # Get the answer for the user inputted question and context list
             with st.spinner("Finding the Answer...") :
                 answer = generate_answer_for_the_question(question, contexts)
 
@@ -129,11 +149,18 @@ elif mode_of_operation == 1 :
             st.header("Answer :")
             st.markdown("---\n" f"{answer}\n\n" "---")
 
+        # If more suggestions are expected, shuffle the contexts to skip the cached entries
+        st.write(" ")
+        if st.button("More suggestions") :
+            random.shuffle(contexts)
+
+        # Use the first 3 contexts of the list for generating questions and answers
         contexts = contexts[:3]
         with st.spinner("Finding some suggestions...") :
             generated_questions_and_contexts = generate_questions(contexts)
             answers = get_answers_for_questions(generated_questions_and_contexts)
 
+        # Print the suggested questions
         st.header("Some suggested questions :")
         st.write(" ")
         question_numbers = list(range(len(generated_questions_and_contexts)))
