9ab577a6b3c90fed21465baa8ac9023a95eafc9e
athlohangade <athlohangade@gmail.com>
2021-05-27 14:43:29 +0530

    Updated the middleware code to get the confidence value score from the backend and pass it to the frontend. Also commented the code.

--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,4 @@
+# Import required libraries
 import sys
 sys.path += ['./']
 from Lib.TextProcessingAndContextCreation import TextProcessingAndContextCreation
@@ -6,12 +7,14 @@ from Lib.QA import QA
 
 class Main :
 
+    # Instantiate the models
     qg = QGPipeline()
     qa = QA()
 
     @classmethod
     def get_contexts_given_the_doc(cls, doc_name) :
 
+        # Call the backend to generate the contexts
         contexts = TextProcessingAndContextCreation.get_context_chunks(doc_name)
         return contexts
 
@@ -20,8 +23,12 @@ class Main :
     def get_questions_given_the_contexts(cls, context_list) :
 
         generated_questions_and_contexts = []
+
+        # Do the following for each context
         for context in context_list :
+            # Generate the questions
             questions = cls.qg(context)
+            # Append the generated questions along with its context to the list
             for j in questions :
                 generated_questions_and_contexts.append([j['question'], context])
 
@@ -32,8 +39,9 @@ class Main :
     def get_answer_given_the_question_and_context_list(cls, question_context_list) :
 
         answers = []
+        # Get the answers. Return only the answer text
         for i in question_context_list :
-            answers.append(cls.qa.answer([i[0]], [i[1]]))
+            answers.append(cls.qa.answer([i[0]], [i[1]])[0])
 
         return answers
 
@@ -41,6 +49,8 @@ class Main :
     @classmethod
     def get_answer_for_single_question_given_context_list(cls, question, context_list) :
 
+        # Duplicate the questions number of contexts times (required for the backend)
         repeated_question_list = [question] * len(context_list)
+        # Get the answers, its corresponding context and the confidence value of the answer
         answer = cls.qa.answer(repeated_question_list, context_list)
         return answer
